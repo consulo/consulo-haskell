7f0664fa668954a2d2f3bc8bc63ae8d3072de12d
VISTALL <vistall.valeriy@gmail.com>
2014-01-13 20:13:18 +0200

    compilation fixed

--- src/org/consulo/haskell/module/extension/HaskellModuleExtensionProvider.java
+++ src/org/consulo/haskell/module/extension/HaskellModuleExtensionProvider.java
@@ -1,45 +1,44 @@
 package org.consulo.haskell.module.extension;
 
-import com.intellij.openapi.module.Module;
-import ideah.HaskellFileType;
+import javax.swing.Icon;
+
+import org.consulo.haskell.HaskellIcons;
 import org.consulo.module.extension.ModuleExtensionProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
+import com.intellij.openapi.module.Module;
 
 /**
  * @author VISTALL
  * @since 13.07.13.
  */
-public class HaskellModuleExtensionProvider implements ModuleExtensionProvider<HaskellModuleExtension, HaskellMutableModuleExtension> {
+public class HaskellModuleExtensionProvider implements ModuleExtensionProvider<HaskellModuleExtension, HaskellMutableModuleExtension>
+{
 	@Nullable
 	@Override
-	public Icon getIcon() {
-		return HaskellFileType.HASKELL_ICON;
+	public Icon getIcon()
+	{
+		return HaskellIcons.Haskell16x16;
 	}
 
 	@NotNull
 	@Override
-	public String getName() {
+	public String getName()
+	{
 		return "Haskell";
 	}
 
 	@NotNull
 	@Override
-	public Class<HaskellModuleExtension> getImmutableClass() {
-		return HaskellModuleExtension.class;
-	}
-
-	@NotNull
-	@Override
-	public HaskellModuleExtension createImmutable(@NotNull String s, @NotNull Module module) {
+	public HaskellModuleExtension createImmutable(@NotNull String s, @NotNull Module module)
+	{
 		return new HaskellModuleExtension(s, module);
 	}
 
 	@NotNull
 	@Override
-	public HaskellMutableModuleExtension createMutable(@NotNull String s, @NotNull Module module, @NotNull HaskellModuleExtension moduleExtension) {
+	public HaskellMutableModuleExtension createMutable(@NotNull String s, @NotNull Module module, @NotNull HaskellModuleExtension moduleExtension)
+	{
 		return new HaskellMutableModuleExtension(s, module, moduleExtension);
 	}
 }
