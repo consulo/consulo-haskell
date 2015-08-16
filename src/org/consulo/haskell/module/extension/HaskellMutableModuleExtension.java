661d886caa191d2cb9886c74aaf41a3bdcf33d4b
VISTALL <vistall.valeriy@gmail.com>
2015-08-16 16:14:48 +0300

    compilation

--- src/org/consulo/haskell/module/extension/HaskellMutableModuleExtension.java
+++ src/org/consulo/haskell/module/extension/HaskellMutableModuleExtension.java
@@ -4,17 +4,20 @@ import javax.swing.JComponent;
 
 import org.consulo.module.extension.MutableModuleExtensionWithSdk;
 import org.consulo.module.extension.MutableModuleInheritableNamedPointer;
-import org.consulo.module.extension.ui.ModuleExtensionWithSdkPanel;
+import org.consulo.module.extension.ui.ModuleExtensionSdkBoxBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.mustbe.consulo.RequiredDispatchThread;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.roots.ModuleRootLayer;
+import com.intellij.util.ui.JBUI;
 
 /**
  * @author VISTALL
  * @since 13.07.13.
  */
-public class HaskellMutableModuleExtension extends HaskellModuleExtension implements MutableModuleExtensionWithSdk<HaskellModuleExtension>
+public class HaskellMutableModuleExtension extends HaskellModuleExtension implements
+		MutableModuleExtensionWithSdk<HaskellModuleExtension>
 {
 	public HaskellMutableModuleExtension(@NotNull String id, @NotNull ModuleRootLayer moduleRootLayer)
 	{
@@ -28,11 +31,13 @@ public class HaskellMutableModuleExtension extends HaskellModuleExtension implem
 		return (MutableModuleInheritableNamedPointer<Sdk>) super.getInheritableSdk();
 	}
 
+	@RequiredDispatchThread
 	@Nullable
 	@Override
 	public JComponent createConfigurablePanel(@Nullable Runnable runnable)
 	{
-		return wrapToNorth(new ModuleExtensionWithSdkPanel(this, runnable));
+		return JBUI.Panels.verticalPanel().addComponent(ModuleExtensionSdkBoxBuilder.createAndDefine(this,
+				runnable).build());
 	}
 
 	@Override
