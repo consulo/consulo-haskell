1510a2eae8d98149c41d3f156fe02e4fb17607f0
VISTALL <vistall.valeriy@gmail.com>
2017-05-09 23:38:13 +0300

    compilation

--- src/ideah/compiler/HaskellCompiler.java
+++ src/ideah/compiler/HaskellCompiler.java
@@ -9,12 +9,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import consulo.haskell.module.extension.HaskellModuleExtension;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.compiler.impl.CompilerUtil;
 import com.intellij.openapi.compiler.CompileContext;
 import com.intellij.openapi.compiler.CompileScope;
-import com.intellij.openapi.compiler.CompilerManager;
 import com.intellij.openapi.compiler.TranslatingCompiler;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.FileTypeManager;
@@ -30,39 +28,50 @@ import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.Chunk;
 import consulo.compiler.impl.resourceCompiler.ResourceCompilerConfiguration;
+import consulo.haskell.module.extension.HaskellModuleExtension;
 import ideah.HaskellFileType;
 import ideah.sdk.HaskellSdkType;
 import ideah.util.CompilerLocation;
 import ideah.util.DeclarationPosition;
 import ideah.util.LineCol;
 
-public final class HaskellCompiler implements TranslatingCompiler {
+public final class HaskellCompiler implements TranslatingCompiler
+{
 
 	private final Project project;
 
-	public HaskellCompiler(Project project) {
+	public HaskellCompiler(Project project)
+	{
 		this.project = project;
 	}
 
-	public boolean isCompilableFile(VirtualFile file, CompileContext context) {
+	public boolean isCompilableFile(VirtualFile file, CompileContext context)
+	{
 		return isCompilableFile(file);
 	}
 
-	public static boolean isCompilableFile(VirtualFile file) {
+	public static boolean isCompilableFile(VirtualFile file)
+	{
 		FileType fileType = FileTypeManager.getInstance().getFileTypeByFile(file);
 		return HaskellFileType.INSTANCE.equals(fileType);
 	}
 
-	public void compile(CompileContext context, Chunk<Module> moduleChunk, VirtualFile[] files, OutputSink sink) {
+	public void compile(CompileContext context, Chunk<Module> moduleChunk, VirtualFile[] files, OutputSink sink)
+	{
 		Map<Module, List<VirtualFile>> mapModulesToVirtualFiles;
-		if (moduleChunk.getNodes().size() == 1) {
+		if(moduleChunk.getNodes().size() == 1)
+		{
 			mapModulesToVirtualFiles = Collections.singletonMap(moduleChunk.getNodes().iterator().next(), Arrays.asList(files));
-		} else {
+		}
+		else
+		{
 			mapModulesToVirtualFiles = CompilerUtil.buildModuleToFilesMap(context, files);
 		}
-		for (Module module : moduleChunk.getNodes()) {
+		for(Module module : moduleChunk.getNodes())
+		{
 			List<VirtualFile> moduleFiles = mapModulesToVirtualFiles.get(module);
-			if (moduleFiles == null) {
+			if(moduleFiles == null)
+			{
 				continue;
 			}
 
@@ -70,18 +79,23 @@ public final class HaskellCompiler implements TranslatingCompiler {
 			List<VirtualFile> toCompile = new ArrayList<VirtualFile>();
 			List<VirtualFile> toCompileTests = new ArrayList<VirtualFile>();
 
-			if (isAcceptableModuleType(module)) {
-				for (VirtualFile file : moduleFiles) {
-					if (shouldCompile(file, project)) {
+			if(isAcceptableModuleType(module))
+			{
+				for(VirtualFile file : moduleFiles)
+				{
+					if(shouldCompile(file, project))
+					{
 						(index.isInTestSourceContent(file) ? toCompileTests : toCompile).add(file);
 					}
 				}
 			}
 
-			if (!toCompile.isEmpty()) {
+			if(!toCompile.isEmpty())
+			{
 				compileFiles(context, module, toCompile, sink, false);
 			}
-			if (!toCompileTests.isEmpty()) {
+			if(!toCompileTests.isEmpty())
+			{
 				compileFiles(context, module, toCompileTests, sink, true);
 			}
 		}
@@ -89,112 +103,119 @@ public final class HaskellCompiler implements TranslatingCompiler {
 
 	@NotNull
 	@Override
-	public FileType[] getInputFileTypes() {
+	public FileType[] getInputFileTypes()
+	{
 		return new FileType[]{HaskellFileType.INSTANCE};
 	}
 
 	@NotNull
 	@Override
-	public FileType[] getOutputFileTypes() {
+	public FileType[] getOutputFileTypes()
+	{
 		return new FileType[]{HiFileType.INSTANCE};
 	}
 
-	static VirtualFile getMainOutput(CompileContext compileContext, Module module, boolean tests) {
-		return tests
-				? compileContext.getModuleOutputDirectoryForTests(module)
-				: compileContext.getModuleOutputDirectory(module);
+	static VirtualFile getMainOutput(CompileContext compileContext, Module module, boolean tests)
+	{
+		return tests ? compileContext.getModuleOutputDirectoryForTests(module) : compileContext.getModuleOutputDirectory(module);
 	}
 
-	private static void compileFiles(CompileContext context, Module module, List<VirtualFile> toCompile,
-									 OutputSink sink, boolean tests) {
-		if (CompilerLocation.get(module) == null)
+	private static void compileFiles(CompileContext context, Module module, List<VirtualFile> toCompile, OutputSink sink, boolean tests)
+	{
+		if(CompilerLocation.get(module) == null)
+		{
 			return; // todo: produce error
+		}
 		VirtualFile outputDir = getMainOutput(context, module, tests);
 		List<OutputItem> output = new ArrayList<OutputItem>();
 		// todo: pass all files to compiler at once (more effective?)
-		for (VirtualFile file : toCompile) {
-			for (GHCMessage message : LaunchGHC.compile(outputDir, file.getPath(), module, tests)) {
+		for(VirtualFile file : toCompile)
+		{
+			for(GHCMessage message : LaunchGHC.compile(outputDir, file.getPath(), module, tests))
+			{
 				VirtualFile errFile = LocalFileSystem.getInstance().findFileByPath(message.getFileName());
 				String url = errFile == null ? message.getFileName() : errFile.getUrl();
 				LineCol coord = message.getRange().start;
-				context.addMessage(
-						message.getCategory(), message.getErrorMessage(),
-						url,
-						coord.line, coord.column
-				);
+				context.addMessage(message.getCategory(), message.getErrorMessage(), url, coord.line, coord.column);
 			}
 		}
 		sink.add(outputDir.getPath(), output, VfsUtil.toVirtualFileArray(toCompile));
 	}
 
-	private static boolean shouldCompile(VirtualFile file, Project project) {
+	private static boolean shouldCompile(VirtualFile file, Project project)
+	{
 		ResourceCompilerConfiguration c = ResourceCompilerConfiguration.getInstance(project);
 		return !c.isResourceFile(file);
 	}
 
-	private static boolean isAcceptableModuleType(Module module) {
+	private static boolean isAcceptableModuleType(Module module)
+	{
 		return ModuleUtilCore.getExtension(module, HaskellModuleExtension.class) != null;
 	}
 
 	@NotNull
-	public String getDescription() {
+	public String getDescription()
+	{
 		return "Haskell compiler";
 	}
 
-	public boolean validateConfiguration(CompileScope compileScope) {
+	public boolean validateConfiguration(CompileScope compileScope)
+	{
 		VirtualFile[] files = compileScope.getFiles(HaskellFileType.INSTANCE, true);
-		if (files.length == 0)
+		if(files.length == 0)
+		{
 			return true;
+		}
 
 		Set<Module> modules = new HashSet<Module>();
-		for (VirtualFile file : files) {
+		for(VirtualFile file : files)
+		{
 			Module module = DeclarationPosition.getModule(project, file);
-			if (module != null) {
+			if(module != null)
+			{
 				modules.add(module);
 			}
 		}
 
 		Set<Module> noGhcModules = new HashSet<Module>();
-		for (Module module : modules) {
-			if (!isAcceptableModuleType(module))
+		for(Module module : modules)
+		{
+			if(!isAcceptableModuleType(module))
+			{
 				continue;
+			}
 			Sdk sdk = ModuleUtilCore.getSdk(module, HaskellModuleExtension.class);
-			if (sdk == null || !(sdk.getSdkType() instanceof HaskellSdkType)) {
+			if(sdk == null || !(sdk.getSdkType() instanceof HaskellSdkType))
+			{
 				noGhcModules.add(module);
 			}
 		}
 
-		if (!noGhcModules.isEmpty()) {
-			if (noGhcModules.size() == 1) {
+		if(!noGhcModules.isEmpty())
+		{
+			if(noGhcModules.size() == 1)
+			{
 				Module module = noGhcModules.iterator().next();
-				Messages.showErrorDialog(
-						project,
-						MessageFormat.format("Cannot compile Haskell files.\nPlease set up GHC for module ''{0}''.", module.getName()),
-						"Cannot Compile"
-				);
-			} else {
+				Messages.showErrorDialog(project, MessageFormat.format("Cannot compile Haskell files.\nPlease set up GHC for module ''{0}''.", module.getName()), "Cannot Compile");
+			}
+			else
+			{
 				StringBuilder buf = new StringBuilder();
 				int i = 0;
-				for (Module module : noGhcModules) {
-					if (i > 0)
+				for(Module module : noGhcModules)
+				{
+					if(i > 0)
+					{
 						buf.append(", ");
+					}
 					buf.append(module.getName());
 					i++;
 				}
-				Messages.showErrorDialog(
-						project,
-						MessageFormat.format("Cannot compile Haskell files.\nPlease set up GHC for modules ''{0}''.", buf.toString()),
-						"Cannot Compile"
-				);
+				Messages.showErrorDialog(project, MessageFormat.format("Cannot compile Haskell files.\nPlease set up GHC for modules ''{0}''.", buf.toString()), "Cannot Compile");
 			}
 			return false;
 		}
 
 		return true;
-	}
-
-	@Override
-	public void init(@NotNull CompilerManager compilerManager) {
-
 	}
 }
