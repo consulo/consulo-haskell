2694b11c58ae0b580b28e75ae9d00abbd10e1dc7
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-10-26 20:53:51 +0300

    compilation

--- src/main/java/ideah/findUsages/HaskellReferencesSearch.java
+++ src/main/java/ideah/findUsages/HaskellReferencesSearch.java
@@ -33,7 +33,7 @@ public final class HaskellReferencesSearch extends QueryExecutorBase<PsiReferenc
     }
 
     @Override
-    public void processQuery(@Nonnull ReferencesSearch.SearchParameters queryParameters, @Nonnull Processor<PsiReference> consumer) {
+    public void processQuery(@Nonnull ReferencesSearch.SearchParameters queryParameters, @Nonnull Processor<? super PsiReference> consumer) {
         PsiElement element = queryParameters.getElementToSearch();
         if (!(element instanceof HPAbstractIdent))
             return;
